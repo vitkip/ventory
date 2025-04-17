769950aa45fb2729bd72f4477f3e306eedc15c6a
vitkip <phathasyla@gmail.com>
2025-04-17 13:40:41 +0700

    edit show.tsx

--- resources/js/pages/Customers/Show.tsx
+++ resources/js/pages/Customers/Show.tsx
@@ -52,7 +52,7 @@ export default function Show({ auth, customer }: ShowProps) {
             <div className="lg:col-span-1">
               <div className="bg-white dark:bg-gray-800 shadow-sm rounded-lg overflow-hidden">
                 <div className="px-6 py-4 border-b border-gray-200 dark:border-gray-700">
-                  <h3 className="text-lg font-semibold text-gray-900 dark:text-gray-100">รูปลูกค้า</h3>
+                  <h3 className="text-lg font-semibold text-gray-900 dark:text-gray-100">ຮູບລູກຄ້າ</h3>
                 </div>
                 <div className="p-6 flex justify-center">
                   <img 
